7935c52c7c6ce8f6af5efb3cd53815d91404ab92
Zenzus <jonas.hein25@gmail.com>
2019-04-16 00:39:27 +0200

    can play a raw mp3 file when you push buttom

--- app/src/main/java/com/example/jonaschrtest_002/MainActivity.kt
+++ app/src/main/java/com/example/jonaschrtest_002/MainActivity.kt
@@ -6,21 +6,59 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.Menu
 import android.view.MenuItem
 
+import android.app.Activity
+import android.media.MediaPlayer
+import android.view.MotionEvent
+
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : Activity() {
+    private  var mediaPlayer:MediaPlayer? = null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setSupportActionBar(toolbar)
+       // setSupportActionBar(toolbar)
 
+
+
+        mediaPlayer = MediaPlayer.create(this,R.raw.explosion)
+        mediaPlayer?.setOnPreparedListener{
+            (println("Ready to go"))
+
+        /*
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
+                */
+        }
+
+        pushButton.setOnTouchListener{ _,event ->
+            handleTouch(event)
+            true
+
+        }
+
+
+    }
+    private fun handleTouch(event: MotionEvent){
+        when (event.action){
+            MotionEvent.ACTION_DOWN ->{
+                println("down")
+                mediaPlayer?.start()
+            }
+            MotionEvent.ACTION_CANCEL,MotionEvent.ACTION_UP -> {
+                println("up or cancel")
+                mediaPlayer?.pause()
+                mediaPlayer?.seekTo(0)
+            }
+            else -> {
+                println("other")
+            }
         }
     }
-
+/*
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.menu_main, menu)
@@ -36,4 +74,5 @@ class MainActivity : AppCompatActivity() {
             else -> super.onOptionsItemSelected(item)
         }
     }
+    */
 }

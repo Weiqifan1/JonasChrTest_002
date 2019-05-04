4a216e4acf2f6182f1da78c20235cd2d1998ba37
Weiqifan1 <cmlykke@hotmail.com>
2019-05-04 12:38:41 +0200

    resetet projektet, herefter sendes tingene ud i sepparate activities

--- app/src/main/java/com/example/jonaschrtest_002/MainActivity.kt
+++ app/src/main/java/com/example/jonaschrtest_002/MainActivity.kt
@@ -19,19 +19,32 @@ class MainActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-       // setSupportActionBar(toolbar)
 
+
+
+    }
+
+
+}
+
+
+/*
+
+class MainActivity : Activity() {
+    private  var mediaPlayer:MediaPlayer? = null
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+       // setSupportActionBar(toolbar)
 
 
         mediaPlayer = MediaPlayer.create(this,R.raw.explosion)
         mediaPlayer?.setOnPreparedListener{
             (println("Ready to go"))
 
-        /*
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-                */
+
         }
 
         pushButton.setOnTouchListener{ _,event ->
@@ -58,21 +71,7 @@ class MainActivity : Activity() {
             }
         }
     }
-/*
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
-        }
-    }
-    */
 }
+
+*/

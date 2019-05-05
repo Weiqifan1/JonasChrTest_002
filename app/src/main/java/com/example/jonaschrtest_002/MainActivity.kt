e6997e9f3a45bcb34ad6a3867a5e7d6cfbe7e190
Zenzus <jonas.hein25@gmail.com>
2019-05-05 20:56:43 +0200

    made a mp3 player with seekbar

--- app/src/main/java/com/example/jonaschrtest_002/MainActivity.kt
+++ app/src/main/java/com/example/jonaschrtest_002/MainActivity.kt
@@ -1,78 +1,109 @@
 package com.example.jonaschrtest_002
 
+import android.content.Context
+import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.support.design.widget.Snackbar
-import android.support.v7.app.AppCompatActivity;
-import android.view.Menu
-import android.view.MenuItem
-
-import android.app.Activity
-import android.media.MediaPlayer
-import android.view.MotionEvent
-
 import kotlinx.android.synthetic.main.activity_main.*
+import android.media.MediaPlayer
+import android.os.Handler
+import android.widget.SeekBar
+import android.widget.Toast
 
-class MainActivity : Activity() {
-    private  var mediaPlayer:MediaPlayer? = null
 
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var player: MediaPlayer
+    private lateinit var runnable:Runnable
+    private var handler: Handler = Handler()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-       // setSupportActionBar(toolbar)
 
+        // Start the media player
+        button_play.setOnClickListener{
+            player = MediaPlayer.create(applicationContext,R.raw.flute)
 
+            player.start()
+            tv_duration.text = "${player.seconds} sec"
+            initializeSeekBar()
 
-        mediaPlayer = MediaPlayer.create(this,R.raw.explosion)
-        mediaPlayer?.setOnPreparedListener{
-            (println("Ready to go"))
+            it.isEnabled = false
+            button_stop.isEnabled = true
 
-        /*
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-                */
-        }
-
-        pushButton.setOnTouchListener{ _,event ->
-            handleTouch(event)
-            true
-
-        }
-
-
-    }
-    private fun handleTouch(event: MotionEvent){
-        when (event.action){
-            MotionEvent.ACTION_DOWN ->{
-                println("down")
-                mediaPlayer?.start()
-            }
-            MotionEvent.ACTION_CANCEL,MotionEvent.ACTION_UP -> {
-                println("up or cancel")
-                mediaPlayer?.pause()
-                mediaPlayer?.seekTo(0)
-            }
-            else -> {
-                println("other")
+            player.setOnCompletionListener {
+                button_stop.isEnabled = false
+                button_play.isEnabled = true
+                toast("end")
             }
         }
-    }
-/*
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
+
+
+        // Stop the media player
+        button_stop.setOnClickListener{
+            if(player.isPlaying){
+                player.stop()
+                player.reset()
+                player.release()
+                handler.removeCallbacks(runnable)
+
+                it.isEnabled = false
+                button_play.isEnabled = true
+            }
+        }
+
+
+        // Seek bar change listener
+        seek_bar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar, i: Int, b: Boolean) {
+                if (b) {
+                    player.seekTo(i * 1000)
+                }
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar) {
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar) {
+            }
+        })
     }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
+
+    // Method to initialize seek bar and audio stats
+    private fun initializeSeekBar() {
+        seek_bar.max = player.seconds
+
+        runnable = Runnable {
+            seek_bar.progress = player.currentSeconds
+
+            tv_pass.text = "${player.currentSeconds} sec"
+            val diff = player.seconds - player.currentSeconds
+            tv_due.text = "$diff sec"
+
+            handler.postDelayed(runnable, 1000)
         }
+        handler.postDelayed(runnable, 1000)
     }
-    */
+}
+
+
+
+// Extension property to get media player duration in seconds
+val MediaPlayer.seconds:Int
+    get() {
+        return this.duration / 1000
+    }
+
+
+// Extension property to get media player current position in seconds
+val MediaPlayer.currentSeconds:Int
+    get() {
+        return this.currentPosition/1000
+    }
+
+
+// Extension function to show toast message quickly
+fun Context.toast(message:String){
+    Toast.makeText(this,message,Toast.LENGTH_SHORT).show()
 }

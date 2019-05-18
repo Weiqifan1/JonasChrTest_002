f7fe542f5e5dc4e63bbeab3f0d3167c3c2b1b02b
Weiqifan1 <cmlykke@hotmail.com>
2019-05-18 18:53:10 +0200

    ny knap til dbtest activity

---
+++ app/src/main/java/com/example/jonaschrtest_002/DbtestActivity.kt
@@ -0,0 +1,21 @@
+package com.example.jonaschrtest_002
+
+import android.app.Activity
+import android.os.Bundle
+
+class DbtestActivity: Activity()  {
+
+    /*
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    */
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dbtest)
+    }
+
+}
--- app/src/main/java/com/example/jonaschrtest_002/MainActivity.kt
+++ app/src/main/java/com/example/jonaschrtest_002/MainActivity.kt
@@ -37,6 +37,10 @@ class MainActivity : Activity() {
             startActivity(Intent(this, AudioFileList::class.java))
         }
 
+        actmain_dbtest.setOnClickListener{
+            startActivity(Intent(this, DbtestActivity::class.java))
+        }
+
 //https://en.proft.me/2017/06/14/runtime-permissions-android-marshmallow-60-and-abo/
         askPermissions(wantedPerm) {
             onGranted {

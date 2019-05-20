8b3486b3eda538338a5fa2d816c913a87bb1a93d
Zenzus <jonas.hein25@gmail.com>
2019-05-20 11:11:35 +0200

    data klassen er lavet og dummy data vises i et textview

--- app/src/main/java/com/example/jonaschrtest_002/Dbhelp.kt
+++ app/src/main/java/com/example/jonaschrtest_002/Dbhelp.kt
@@ -2,11 +2,26 @@ package com.example.jonaschrtest_002
 
 import android.app.Activity
 import android.os.Bundle
+import com.example.jonaschrtest_002.Database.Bookmark
+import com.example.jonaschrtest_002.Database.bookmarks
+import kotlinx.android.synthetic.main.activity_dbhelp.*
 
 class Dbhelp: Activity()  {
+
+    val bookmarkList: List<Bookmark> = bookmarks
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dbhelp)
+
+
+
+        var displayBookmarks = ""
+        for (item in bookmarkList) {
+            displayBookmarks += "\n\n" + item.toString()
+        }
+
+        dbhelp_text.text = displayBookmarks
 
     }
 }

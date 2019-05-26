ed17023248dcf25170f4fc973a0d8d6b5db41cf3
Weiqifan1 <cmlykke@hotmail.com>
2019-05-26 14:44:12 +0200

    copy Anders kode igen

--- app/src/main/java/com/example/jonaschrtest_002/Database/Tables.kt
+++
@@ -1,8 +0,0 @@
-package com.example.jonaschrtest_002.Database
-
-object CityForecastTable {
-    const val NAME = "CityForecast"
-    const val ID = "_id"
-    const val CITY = "city"
-    const val COUNTRY = "country"
-}
---
+++ app/src/main/java/com/example/jonaschrtest_002/Database/Vets.kt
@@ -0,0 +1,4 @@
+package com.example.jonaschrtest_002.Database
+
+open class Pet(val id: Int, var name: String, var alive: Boolean = true) {
+}
